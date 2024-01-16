f76a396d9e8c37ce8a0ae28ab558b1a2ae0015d4
Swadhindhara <swadhindhara@gmail.com>
2024-01-16 23:24:07 +0530

    image url passing through variable in nav section

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 
 function Nav() {
   const [expanded, setExpanded] = useState(false);
+
+  const image = "./public/assets/logo.png"
+
   return (
     <>
       <div className="nav">
         <div className="left_nav">
           <Link to="/">
-            <img src="./public/assets/logo.png" alt="" />
+            <img src={image} alt="" />
           </Link>
         </div>
         <div className={`mid_nav ${expanded ? "cus-nav-mobile" : ""}`}>
@@ -70,11 +73,11 @@ function Nav() {
               width="24"
               height="24"
               fill="currentColor"
-              class="bi bi-list"
+              className ="bi bi-list"
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
               />
             </svg>
